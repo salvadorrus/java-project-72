122b462bee7b9b2bceaa8b99f1e26e6f8e982cbf
salvadorrus <ntvkras@list.ru>
2024-12-18 15:05:46 +0300

    Add BaseRepository, urls

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,13 +3,25 @@ package hexlet.code;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class App {
-    public static void getApp() {
+    public static Javalin getApp() {
+
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+
+//        var dataSource = new HikariDataSource(hikariConfig);
+//        BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
         });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
     }
 
     public static void main(String[] args) {
